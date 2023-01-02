391b982448f76b8a562ccb9a98041e03ef20c6f0
Vahid Alizadeh <vhoseinalizadeh@gmail.com>
2023-01-02 11:08:11 +0330

    home_view completed

--- lib/screens/home/home_view.dart
+++ lib/screens/home/home_view.dart
@@ -8,155 +8,90 @@ import 'package:signalr_client/screens/chat/widgets/message.dart';
 import '/core/dependency_injection.dart';
 // import '/widgets/LoadingWidget.dart';
 // import '/widgets/my_app_bar.dart';
-import 'chat_controller.dart';
+import 'home_controller.dart';
 // import '../widgets/drawer_widget.dart';
 // import '../widgets/home_header.dart';
 // import '../widgets/home_list_widget.dart';
 
-class ChatView extends StatelessWidget {
-  final ChatController myController = getIt<ChatController>();
+class HomeView extends StatelessWidget {
+  final HomeController myController = getIt<HomeController>();
 
-  ChatView({required this.chatKey, Key? key}) : super(key: key);
-
-  final String chatKey;
+  HomeView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    debugPrint("before controller init");
-    myController.onInit(args: chatKey);
-    debugPrint("after controller init");
+    int imageIndex = -1;
     return SafeArea(
-      child: Stack(
-        children: [
-          Image.asset(
-            "assets/images/chatbackwhatsapp.png",
-            height: MediaQuery.of(context).size.height,
-            width: MediaQuery.of(context).size.width,
-            fit: BoxFit.cover,
-          ),
-          Scaffold(
-              backgroundColor: Colors.transparent,
-              appBar: AppBar(
-                // bottom: TabBar(
-                //   tabs: tabs,
-                //   controller: tabController,
-                // ),
-                  leading: IconButton(
-                    onPressed: () {
-                      myController.bachToHomeScreen();
-                    },
-                    icon: Icon(Icons.arrow_back, color: Colors.white),
-                  ),
-                  title: Text(
-                    "My Id is ${myController.homeState.myId}",
-                    style: const TextStyle(fontSize: 20, color: Colors.white),
-                  )),
-              body:
-              // myController.homeState.rebuildChatList.value;
-              Stack(
-                children: [
-                  Obx(() {
-                    debugPrint("rebuild this chat");
-                    myController.chatState.rebuildChatList.value;
-                    return Container(
-                      height: MediaQuery.of(context).size.height - 140,
-                      child: ListView(
-                        shrinkWrap: true,
-                        children: [
-                          ...myController.chat!.messages.map((e) {
-                            return MessageWidget(
-                              messageText:
-                              e.text ,
-                              clientMessage: e.sender == myController.homeState.myId,
-                              chatType: myController.chat!.type,
-                              senderId: e.sender,
-                              date: e.date!,
-                              senderUserName: e.senderUserName,
-                            );
-                          }).toList()
-                        ],
-                      ),
-                    );}),
-                  Align(
-                    alignment: Alignment.bottomCenter,
-                    child: Obx( () {
-                      myController.chatState.showSendMessageIcon.value;
-                      return Row(
-                        children: [
-                          Container(
-                            width: MediaQuery.of(context).size.width - 55,
-                            child: Card(
-                              margin: const EdgeInsets.only(left: 2, right: 2, bottom: 8),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(25),
-                              ),
-                              child: TextFormField(
-                                textAlignVertical: TextAlignVertical.center,
-                                keyboardType: TextInputType.multiline,
-                                maxLines: 5,
-                                minLines: 1,
-                                decoration: InputDecoration(
-                                    border: InputBorder.none,
-                                    prefixIcon: IconButton(
-                                      icon: Icon(Icons.emoji_emotions),
-                                      onPressed: () {},
-                                    ),
-                                    suffixIcon: Row(
-                                      mainAxisSize: MainAxisSize.min,
-                                      children: [
-                                        IconButton(
-                                          onPressed: () {},
-                                          icon: Icon(Icons.attach_file),
-                                          padding: EdgeInsets.zero,
-                                        ),
-                                        myController.textController.text == "" ?   IconButton(
-                                            onPressed: () {
-                                              if(myController.textController.text  == ""){
-                                                debugPrint("what is this");
-                                              }
-
-                                            }, icon: Icon(Icons.camera_alt), padding: EdgeInsets.zero) : SizedBox(height: 0, width: 0,)
-                                      ],
-                                    ),
-                                    contentPadding: EdgeInsets.all(10),
-                                    hintText: "Message"),
-                                controller: myController.textController,
-                                onChanged: (str){
-                                  //todo move this code to controller
-                                  if(str ==  ""  && myController.chatState.showSendMessageIcon.value){
-                                    myController.chatState.showSendMessageIcon.toggle();
-                                  }else if(str != "" && !myController.chatState.showSendMessageIcon.value){
-                                    myController.chatState.showSendMessageIcon.toggle();
-                                  }
-                                },
-                              ),
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(left: 2, right: 2, bottom: 8),
-                            child: CircleAvatar(
-                              radius: 25,
-                              child: myController.textController.text == "" ?  IconButton(onPressed: () {}, icon: Icon(Icons.mic)) :
-                              IconButton(
-                                  onPressed: () => myController.chat?.type == ChatType.contact ? myController.sendMessageToContact() :
-                                  myController.sendMessageToGroup()
-                                  , icon: Icon(Icons.send)),
-                            ),
-                          )
-                        ],
-                      ); }),
-                  )
-                ],
-              )
-
-            // Obx(() => myController.homeState.homeLoading.value
-            //     ? const LoadingWidget()
-            //     : SingleChildScrollView(
-            //     padding: const EdgeInsets.only(bottom: 100),
-            //     child: HomeListWidget(listFlights: myController.showFlightList()))),
-          )
+        child: Scaffold(
+      backgroundColor: Colors.transparent,
+      appBar: AppBar(
+        // bottom: TabBar(
+        //   tabs: tabs,
+        //   controller: tabController,
+        // ),
+        //   leading: IconButton(
+        //     onPressed: () {
+        //       myController.bachToHomeScreen();
+        //     },
+        //     icon: Icon(Icons.arrow_back, color: Colors.white),
+        //   ),
+        title: Text(
+          "My Id is ${myController.homeState.myId}",
+          style: const TextStyle(fontSize: 20, color: Colors.white),
+        ),
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.search,
+                color: Colors.white,
+              )),
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.more_vert,
+                color: Colors.white,
+              ))
         ],
       ),
-    );
+      body:
+          // myController.homeState.rebuildChatList.value;
+
+          Obx(() {
+        debugPrint("rebuild this chat");
+        imageIndex = -1;
+        myController.homeState.rebuildChatList.value;
+        return ListView(
+          shrinkWrap: true,
+          children: [
+            ...myController.homeState.chats.map((e) {
+              imageIndex++;
+              return Row(
+                children: [
+                  CircleAvatar(
+                    backgroundImage: AssetImage("assets/images/$imageIndex.jpg"),
+                    radius: 25,
+                  ),
+                  Column(
+                    children: [
+                      Text(e.userName!),
+                      Text(e.messages != [] ? "${e.messages[0].senderUserName}:${e.messages[0].text}" : ""),
+                      const Text(
+                        "19:54",
+                        style: TextStyle(color: Colors.grey),
+                      )
+                    ],
+                  )
+                ],
+              );
+            }).toList()
+          ],
+        );
+      }),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => myController.goToNewChatScreen(),
+        child: const Icon(Icons.chat),
+      ),
+    ));
   }
 }
